3d972623b4340bd08ee53fd8a16b6a2d233bd621
tienan.chien <tienan.chien@iisigroup.com>
2021-02-24 17:01:46 +0800

    init

---
+++ src/main/java/com/example/sagase/SagaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.sagase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sagase/SagaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sagase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
